4dd127fb400c455d8fcd1f81baee926e63f222be
Mohammad reza <mr.sharifi1375@gmail.com>
2020-03-18 18:36:22 +0330

    db population via excel, and normalization

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from pandas import read_excel
+import sqlite3
 import config
 
 app = Flask(__name__)
@@ -13,7 +14,7 @@ def process():
     data = request.form
     # import pdb; pdb.set_trace()
     sender = data["from"]
-    message = data["message"]
+    message = normalize_string(data["message"])
     print(f'message {message} recieved from {sender}')
     send_sms(sender, 'Hi '+message)
     ret =  {"message": "processed"}
@@ -30,20 +31,84 @@ def send_sms(receptor, message):
     print(f"message *{message}* sent. status code os {res.status_code}")
 
 
+def normalize_string(str):
+    from_char = '۰۱۲۳۴۵۶۷۸۹'
+    to_char = '0123456789'
+    for i in range(len(from_char)):
+        str = str.replace(from_char[i], to_char[i])
+    str = str.upper()
+    return(str)
+
+
+
+
 
 def import_database_from_excel(filepath):
-    """gets an excel file name and imports lookup data (data and failures) from it """
+    """gets an excel file name and imports lookup data (data and failures) from it 
+        the (1) sheet contains serial data like:
+         Row	Reference_Number	Description	Start_Serial	End_Serial	Date
+         and the (0) contains a column of invalid serials.
+         This data will be wrriten into the qlite database located at config.DATABASE_FILE_PATH
+        in two tables."serials" and "invalids" 
+        
+        return two integers: (number of serial rows, number of invalid rows)
+        
+        """
     # df contains lookup data in the form of
+
     # Row	Reference Number	Description	Start Serial	End Serial	Date
-    df = read_excel(filepath, 1)
+    
+    # TODO: make sure that the data is imported correctly, we need to backup the old one.
+    # TODO: do some normalization
+    ## our sqlite database will contain two tables: serials and invalids
+    conn = sqlite3.connect(config.DATABASE_FILE_PATH)
+    cur = conn.cursor()
+    # remove the serials table if exists, then create new one
+    cur.execute('DROP TABLE IF EXISTS serials')
+    cur.execute("""CREATE TABLE IF NOT EXISTS serials (
+        id INTEGER PRIMARY KEY,
+        ref TEXT,
+        desc TEXT,
+        start_serial TEXT,
+        end_serial TEXT,
+        date DATE);""")
+    
+    df = read_excel(filepath, 0)
+    serial_counter = 0
     for index,(line, ref, desc, start_serial, end_serial, date) in df.iterrows():
-        print(line, ref, desc, start_serial, end_serial, date)
+        start_serial = normalize_string(start_serial)
+        end_serial = normalize_string(end_serial)
+        query = f'INSERT INTO serials (id, ref, desc, start_serial, end_serial, date) VALUES("{line}", "{ref}", "{desc}", "{start_serial}", "{end_serial}", "{date}");'
+        cur.execute(query)
+        # TODO: do some more error handling
+        if serial_counter % 10 == 0:
+            conn.commit()
 
-    df = read_excel(filepath, 0) # sheet 1 contain failed serial numbers.only one column  exists.
-    for index, (failed_serial) in df.iterrows():
-        print(failed_serial[0])
+        serial_counter += 1
+        #print(line, ref, desc, start_serial, end_serial, date)
+    conn.commit()
 
+     # remove the invalids table if exists, then create new one
+    cur.execute('DROP TABLE IF EXISTS invalids')
+    cur.execute("""CREATE TABLE IF NOT EXISTS invalids (
+        invalid_serial TEXT PRIMARY KEY);""")
+    conn.commit()
+    # now lets save the invalid serials
+    df = read_excel(filepath, 1) # sheet 1 contain failed serial numbers.only one column  exists.
+    invalid_counter = 0
+    for index, (failed_serial, ) in df.iterrows():
+        #failed_serial = failed_serial_row[0]
 
+        query = f'INSERT INTO invalids VALUES("{failed_serial}");'
+        cur.execute(query)
+        # TODO: do some more error handling
+        if invalid_counter % 10 == 0:
+            conn.commit()
+
+        invalid_counter += 1
+    conn.commit()
+    cur.close()
+    return (serial_counter, invalid_counter)
 def check_serial():
     pass
 
@@ -51,4 +116,5 @@ def check_serial():
 if __name__ == "__main__":
     # send_sms('000', 'erfvs')
     #app.run("0.0.0.0", 5000, debug=True)
-    import_database_from_excel('data.xlsx')
+    a,b = import_database_from_excel('data.xlsx')
+    print(f'inserted {a} rows and {b} invalids')
