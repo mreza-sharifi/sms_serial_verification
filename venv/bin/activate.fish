f35a729c9467c28c74a3e7b82b64c4612f1c4c40
sharifi1375 <mr.sharifi1375@gmail.com>
2020-03-19 22:44:55 +0330

    Dockerizing

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -57,7 +57,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/home/mohammad/Documents/Projects/SMS_Verify/venv'
+set -gx VIRTUAL_ENV '/home/mohammad/Documents/Projects/sms_serial_verification/venv'
 
 # https://github.com/fish-shell/fish-shell/issues/436 altered PATH handling
 if test (echo $FISH_VERSION | head -c 1) -lt 3
